31672de03430efc0d75dfdf91f3b6a82a7466579
AmithBadesha <badesha_a07@hotmail.com>
2013-11-25 18:19:15 -0800

    adding lab1

---
+++ Lab1/src/p1/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package p1;
+
+/**
+ * @author Owner
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+         System.out.println("Hello, world");
+    }
+
+}
